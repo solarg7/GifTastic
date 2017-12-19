734495b970251f51d30935ed21fd8d1de8e67303
Del Solar <marcelo.delsolar@siemens.com>
2017-12-19 08:41:25 -0500

    HW6

--- giftcat.js
+++ giftcat.js
@@ -3,7 +3,7 @@ window.onload = function() {
 	$(document).ready(function() {
 
 		
-		//
+		//animals array
 		var animals = ["monkey", "elefant", "dog"];
 
 		buttonAddAnimal();
@@ -25,6 +25,8 @@ window.onload = function() {
 				//add a class: "animal"
 				buttonAux.addClass("animal");
 
+
+				// add bootstrap button style
 				buttonAux.addClass("btn btn-default");
 
 				//Add a data-attribute with the value of the animals at index i
@@ -39,12 +41,13 @@ window.onload = function() {
 				$("#buttonContainer"). append(buttonAux);
 				
 			}
+			//call function to get 10 gifs with an API
 			secondAction();
 		}
 
 
 		
-		// body...
+		// function to generate and put the new topic-button on the buttons...
 	
 		$("#add-animal").on("click", function(event){
 				//prevent the form from trying to submit itself.
@@ -57,34 +60,32 @@ window.onload = function() {
 				animals.push(animal);
 				
 
-				//call buttonAddAnimal
+				//call buttonAddAnimal to built all buttons on the buttons area
 				buttonAddAnimal();
-				//check how clean the text-input
+
+				//clean the text-input
 				$(".inText").val('');
 		});
 
-		// Call the buttonAddAnimal function at least once to display the initial list of movies
-		//buttonAddAnimal();
-
 		
+		//a function gets data with API and puts gifs on the gifs area		
 		function secondAction() {
 		
 			//event listener for all created animal buttons
 			$(".animal").on("click", function(){
 
-				console.log("hola61");
-
+				//clean the gifs area before load a new gifs
 				$("#gifContainer").empty();
 
 
 
 
 
-				//the "this" keyword refers to the button that was clicked
+				//the "this" keyword refers to the button that was clicked and get data-name from it
 				var animalCall = $(this).attr("data-name");
 
 
-				//animalCall = $(this).attr("data-name");
+				
 
 				//built a URL to search Giphy for the animals from the quote
 				var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -97,47 +98,63 @@ window.onload = function() {
 		          })
 		          // After the data comes back from the API
 		          .done(function(response) {
-		            	// Storing an array of results in the results variable
+		            	// Storing an array of results in the listResults variable
 		            	var listResult = [];
 						listResult = response.data;
 						console.log(listResult);
 
+
+						// a loop to generate and put gifs on the gifs area
 			        	for (var i=0; i < listResult.length; i++){
 				          	if(listResult[i].rating !== "r" && listResult[i].rating !== "pg-13"){
 
+
+				          		// generate a <a> variable
 				          		var gifDivContainer = $("<a>");
+
+				          		//read the rating from array
 				          		var rating = listResult[i].rating;
 
+				          		// generate a <div> variable p with rating values
 				          		var p = $("<div>").text("Rating: " + rating);
 
+
+				          		//add a class to define a fixed width of rating info banner
 				          		p.addClass("widthRating");
 
+
+				          		//generate a <image> variable amimalImage
 				          		var animalImage = $("<img>");
 
+				          		//add class to gifDivContainer
 				          		gifDivContainer.addClass("gifCss");
 
+
+								//add a class to animalImage
 				          		animalImage.addClass("gifAction");
 
-				          		animalImage.attr("data-state", "still");
-								
+
+				          		//add another attributes to animalImage from array
+				          		animalImage.attr("data-state", "still");								
 
 								animalImage.attr("data-still", listResult[i].images.fixed_height_still.url);
 
 								animalImage.attr("data-animated", listResult[i].images.fixed_height.url);
 
-
-
-
 				          		animalImage.attr("src", listResult[i].images.fixed_height_still.url);
 
+
+				          		//preparing a gif container in to memory
 				          		gifDivContainer.append(p);
 				          		gifDivContainer.append(animalImage);
 
 								
 
-
+				          		//add a gif container in to gifs area
 				          		$("#gifContainer").prepend(gifDivContainer);
 
+
+				          		//a function is waiting for still or animate the clicked gif 
 				          		thirdAction ();
 
 				        	}
@@ -149,15 +166,20 @@ window.onload = function() {
 			});
 		}
 
-
+		//a function is waiting for still or animate the clicked gif 
 		function thirdAction (){
 
 			$(".gifAction").on("click", function() {
 		      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+		      
+
+		      //listen status of gif
 		      var state = $(this).attr("data-state");
 		      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
 		      // Then, set the image's data-state to animate
 		      // Else set src to the data-still value
+
+		      //conditional for change gif status
 		      if (state === "still") {
 		        $(this).attr("src", $(this).attr("data-animated"));
 		        $(this).attr("data-state", "animate");
